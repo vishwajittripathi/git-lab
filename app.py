2a8cbc43d9fcf1a21cbec4d1de5cfae263533a50
vishwajit tripathi <tripathi.vishwajit@gmail.com>
2025-11-01 09:14:59 +0000

    Add main exclusive line

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
 print('Hello Git')
-print('This is a feature branch')
-print('Update from local machine')
-name = input('Enter your name:')
-print(f'Hello {name}')
-print('reupdate for pull with feature branch')
-print ('recheck pull remote request')
-# New feature implementation
-print ('New feature implementation')
-# new line
-# line 11
-# line 12
-print('Main branch change - version 1')
